543b285359f2a33812fffbc0ae371e00c19dd5da
leiwei1991 <523500710@qq.com>
2018-06-21 11:31:28 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ module.exports = function (app) {
   app.get('/', function (req, res) {
     res.redirect('/articles')
   })
-  app.use('/signup', require('./signup'))
   app.use('/signin', require('./signin'))
   app.use('/signout', require('./signout'))
   app.use('/articles', require('./articles'))
